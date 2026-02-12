cbdadfca824fc4c9d3c1cb211397f4f857c27031
Sharmistha21 <sharmistha.chak21@gmail.com>
2026-02-12 20:00:26 +0530

    use state components done

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName,setFirstName]=useState("");
+  const [lastName,setLastName]=useState("");
+  const [email,setEmail]=useState("");
+  const [contact,setContact]=useState("");
+  const [gender,setGender]=useState("male");
+  const [bestSubject,setBestSubject]=useState({
+    English:true,
+    Maths:false,
+    Physics:false
+  })
+
+  const [resume,setResume]=useState("");
+  const [url,setUrl]=useState("");
+  const [choice,setChoice]=useState("");
+  const [about,setAbout]=useState("");
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(
+      firstName,
+      lastName,
+      email,
+      contact,
+      gender,
+      bestSubject,
+      resume,
+      url,
+      choice,
+      about
+    );
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+       
     </>
   )
 }
