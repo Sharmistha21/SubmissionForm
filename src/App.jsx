90b842d9984380ca0b2e2999d12ae2c7c15f9daf
Sharmistha21 <sharmistha.chak21@gmail.com>
2026-02-12 21:30:48 +0530

    radio button

--- src/App.jsx
+++ src/App.jsx
@@ -36,11 +36,66 @@ function App() {
     );
   };
 
+  const handleSubjectChange=(sub)=>{
+    setBestSubject((prev)=>({
+         ...prev,[sub]:!prev[sub]
+    }))
+  }
+
+  const handleRest=()=>{
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setContact("")
+    setGender("male");
+    setBestSubject({
+      English:true,
+      Maths:false,
+      Physics:false
+    });
+    setResume("");
+    setUrl("");
+    setChoice("");
+    setAbout("");
+  }
+
   return (
-    <>
-      
+    <div className="App">
+      <h1>Form in React</h1>
+      <fieldset>
+        <form action="#" method='GET'>
+          <label htmlFor="firstname">
+            First Name*
+          </label>
+          <input type="text" name="firstname" id="firstname" value={firstName} onChange={(e)=>setFirstName(e.target.value)} placeholder='Enter First Name' required/>
+          <label htmlFor="lastname">
+            Last Name*
+          </label>
+          <input type="text" name="lastname" id="lastname" value={lastName} onChange={(e)=>setLastName(e.target.value)} placeholder='Enter Last Name'/>
+          <label htmlFor="email">
+             Enter Email*
+          </label> 
+          <input type="email" name="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Enter email' required/>
+          <label htmlFor="tel">
+            Contact*
+          </label>
+          <input type="tel"name="contact" id="contact" value={contact} onChange={(e)=>setContact(e.target.value)} placeholder='Enter Mobile number' required/> 
+          <label htmlFor="gender">
+            Gender*
+          </label>
+          
+          <input type="radio" name="gender" id="male" value="male" checked={gender==="male"} onChange={(e)=>setGender(e.target.value)}/>
+           Male
+            <input type="radio" name="gender" id="female" value="female" checked={gender==="female"} onChange={(e)=>setGender(e.target.value)}/>
+           Female
+           <input type="radio" name="gender" id="other" value="other" checked={gender==="other"} onChange={(e)=>setGender(e.target.value)}/>
+           Other
+
+
+        </form>
+      </fieldset>
        
-    </>
+    </div>
   )
 }
 
